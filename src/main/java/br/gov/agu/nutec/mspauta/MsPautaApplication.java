2fe359e54e003404745ce3361b675c0652ef311b
Danilo Chaves <danchavesds@gmail.com>
2025-09-16 12:48:34 -0300

    first commit

---
+++ src/main/java/br/gov/agu/nutec/mspauta/MsPautaApplication.java
@@ -0,0 +1,13 @@
+package br.gov.agu.nutec.mspauta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsPautaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsPautaApplication.class, args);
+    }
+
+}
