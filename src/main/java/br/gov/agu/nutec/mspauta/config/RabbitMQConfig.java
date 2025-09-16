eae6f9ed6f1185134a2643e69168a1d972e6d5e9
Danilo Chaves <danchavesds@gmail.com>
2025-09-16 12:56:18 -0300

    Adicionando configurações do RabbitMQ e do application.properties

---
+++ src/main/java/br/gov/agu/nutec/mspauta/config/RabbitMQConfig.java
@@ -0,0 +1,38 @@
+package br.gov.agu.nutec.mspauta.config;
+
+import org.springframework.amqp.core.*;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${rabbitmq.exchange.audiencia-pendente}")
+    private String exchangeAudienciaPendente;
+
+    @Value("${rabbitmq.queue.sollux-ms-pauta}")
+    private String filaAudienciaPendenteMsPace;
+
+    @Bean
+    public MessageConverter messageConverter(){
+        return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public FanoutExchange fanoutExchange(){
+        return ExchangeBuilder.fanoutExchange(exchangeAudienciaPendente).build();
+    }
+
+    @Bean
+    public Queue filaAudienciaPendenteMsPauta(){
+        return QueueBuilder.durable(filaAudienciaPendenteMsPace).build();
+    }
+
+    @Bean
+    public Binding bindingFilaAudienciaPendenteMsPace(FanoutExchange fanoutExchange, Queue filaAudienciaPendenteMsPauta){
+        return BindingBuilder.bind(filaAudienciaPendenteMsPauta).to(fanoutExchange);
+    }
+}
